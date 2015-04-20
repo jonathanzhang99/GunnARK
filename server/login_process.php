67a03519c33a9937f30c14c6ba391e52965e0f89
Jonathan Zhang <jonathan.z99@gmail.com>
2015-04-19 22:40:46 -0700

    testing4

--- server/login_process.php
+++ server/login_process.php
@@ -16,7 +16,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])){
 	);
 	$account = $accounts->findOne($query);
 	if ($account) {
-		if (password_verify($password, $account["password"])) {
+		if (password_verify($password, $account['password'])) {
 			$_SESSION['user_login'] = $email;
 			$_SESSION['team_name'] = $account["team_name"];
 			$_SESSION['name_member_1'] = $account["name_member_1"];
